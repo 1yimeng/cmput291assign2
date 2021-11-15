3049038d4dc7763c47c9c1d57bf330c5c3709c2b
rachel-ellis <70451233+rachel-ellis@users.noreply.github.com>
2021-11-14 20:34:18 -0700

    fixed name of small db and edited file to create the small, medium or large db (as desired)

--- 291AssignCreateDB.py
+++ 291AssignCreateDB.py
@@ -2,7 +2,8 @@ import pandas as pd
 import sqlite3
 
 
-conn = sqlite3.connect(r"smallDB.db")
+# A3Small.db      A3Medium.db     A3Large.db
+conn = sqlite3.connect(r"A3Large.db")
 cur = conn.cursor()
 
 # create tables as specified in the assignment description
@@ -18,23 +19,23 @@ cur.execute('''CREATE TABLE Order_items (order_id TEXT,	order_item_id INTEGER, p
 customers = pd.read_csv(r"A3_CSV_files\olist_customers_dataset.csv")
 customers = customers.loc[:, ['customer_id', 'customer_zip_code_prefix']].rename(
     columns={"customer_zip_code_prefix": "customer_postal_code"})
-customers = customers.sample(n=10000)
+customers = customers.sample(n=33000)  # 10000    20000   33000
 customers.to_sql('Customers', conn, if_exists='append', index=False)
 
 sellers = pd.read_csv(r"A3_CSV_files\olist_sellers_dataset.csv")
 sellers = sellers.loc[:, ['seller_id', 'seller_zip_code_prefix']].rename(
     columns={"seller_zip_code_prefix": "seller_postal_code"})
-sellers = sellers.sample(n=500)
+sellers = sellers.sample(n=1000)  # 500     750   1000
 sellers.to_sql('Sellers', conn, if_exists='append', index=False)
 
 orders = pd.read_csv(r"A3_CSV_files\olist_orders_dataset.csv")
 orders = orders.loc[:, ['order_id', 'customer_id']]
-orders = orders.sample(n=10000)
+orders = orders.sample(n=33000)  # 10000    20000     33000
 orders.to_sql('Orders', conn, if_exists='append', index=False)
 
 items = pd.read_csv(r"A3_CSV_files\olist_order_items_dataset.csv")
 items = items.loc[:, ['order_id', 'order_item_id', 'product_id', 'seller_id']]
-items = items.sample(n=2000)
+items = items.sample(n=10000)  # 2000     4000     10000
 items.to_sql('Order_items', conn, if_exists='append', index=False)
 
 conn.close()
